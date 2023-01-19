38cbcf61af827e0778582a236a8db0a335bd1970
Parul Pathak <83744951+Parulpathakkk@users.noreply.github.com>
2023-01-19 21:40:44 +0530

    Update App.jsx
    fixed the browerRouter error

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = ()=> {
  
     return (
       <div>
-        <BrowserRouter basename='/'>
+        <BrowserRouter basename='/newsEra'>
         <Navbar/> 
         <LoadingBar
         height={3}
